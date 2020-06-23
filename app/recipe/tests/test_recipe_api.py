2da04320503134c2c3f9d07847688b18cdeeb87b
Harold Mendoza <hgmendoza25@gmail.com>
2020-06-23 21:50:03 +0800

    added feature filter recipes

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -221,25 +221,24 @@ class PrivateRecipeApiTests(TestCase):
         self.assertEqual(len(tags), 1)
         self.assertIn(new_tag, tags)
 
+    def test_full_update_recipe(self):
+        """Test updating a recipe with put"""
+        recipe = sample_recipe(user=self.user)
+        recipe.tags.add(sample_tag(user=self.user))
+        payload = {
+            'title': 'Spaghetti carbonara',
+            'time_minutes': 25,
+            'price': 5.00
+        }
+        url = detail_url(recipe.id)
+        self.client.put(url, payload)
 
-def test_full_update_recipe(self):
-    """Test updating a recipe with put"""
-    recipe = sample_recipe(user=self.user)
-    recipe.tags.add(sample_tag(user=self.user))
-    payload = {
-        'title': 'Spaghetti carbonara',
-        'time_minutes': 25,
-        'price': 5.00
-    }
-    url = detail_url(recipe.id)
-    self.client.put(url, payload)
-
-    recipe.refresh_from_db()
-    self.assertEqual(recipe.title, payload['title'])
-    self.assertEqual(recipe.time_minutes, payload['time_minutes'])
-    self.assertEqual(recipe.price, payload['price'])
-    tags = recipe.tags.all()
-    self.assertEqual(len(tags), 0)
+        recipe.refresh_from_db()
+        self.assertEqual(recipe.title, payload['title'])
+        self.assertEqual(recipe.time_minutes, payload['time_minutes'])
+        self.assertEqual(recipe.price, payload['price'])
+        tags = recipe.tags.all()
+        self.assertEqual(len(tags), 0)
 
 
 class RecipeImageUploadTests(TestCase):
@@ -282,3 +281,51 @@ class RecipeImageUploadTests(TestCase):
             url, {'image': 'notimage'}, format='multipart')
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_filter_recipes_by_tags(self):
+        """
+        Test returning recipes with specific tags
+        """
+        recipe1 = sample_recipe(user=self.user, title='Thai vegetable curry')
+        recipe2 = sample_recipe(user=self.user, title='Aubergine with tahini')
+        tag1 = sample_tag(user=self.user, name='Vegan')
+        tag2 = sample_tag(user=self.user, name='Vegetarian')
+        recipe1.tags.add(tag1)
+        recipe2.tags.add(tag2)
+        recipe3 = sample_recipe(user=self.user, title='Fish and 123')
+
+        response = self.client.get(
+            RECIPES_URL,
+            {'tags': f'{tag1.id},{tag2.id}'}
+        )
+
+        serializer1 = RecipeSerializer(recipe1)
+        serializer2 = RecipeSerializer(recipe2)
+        serializer3 = RecipeSerializer(recipe3)
+        self.assertIn(serializer1.data, response.data)
+        self.assertIn(serializer2.data, response.data)
+        self.assertNotIn(serializer3.data, response.data)
+
+    def test_filter_recipes_by_ingredients(self):
+        recipe1 = sample_recipe(user=self.user, title='Posh beans on toast')
+        recipe2 = sample_recipe(user=self.user, title='Chicken cacciatore')
+        ingredient1 = sample_ingredient(user=self.user, name='Feta Cheese')
+        ingredient2 = sample_ingredient(user=self.user, name='Chicken')
+        recipe1.ingredients.add(ingredient1)
+        recipe2.ingredients.add(ingredient2)
+        recipe3 = sample_recipe(user=self.user, title='Steak and Mushroom')
+
+        response = self.client.get(
+            RECIPES_URL,
+            {
+                'ingredients': f'{ingredient1.id},{ingredient2.id}'
+            }
+        )
+
+        serializer1 = RecipeSerializer(recipe1)
+        serializer2 = RecipeSerializer(recipe2)
+        serializer3 = RecipeSerializer(recipe3)
+
+        self.assertIn(serializer1.data, response.data)
+        self.assertIn(serializer2.data, response.data)
+        self.assertNotIn(serializer3.data, response.data)
